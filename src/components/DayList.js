2b4d6487724f0b81064e9433b6c1ce96976f1290
Mehmet Yuksel <mh.mehmetyuksel@gmail.com>
2021-09-23 13:22:07 +0300

    Initial Commit

---
+++ src/components/DayList.js
@@ -0,0 +1,66 @@
+
+import React from 'react'
+import '../index.css'
+
+class DayList extends React.Component {
+
+
+    state = {
+        city_name: ""
+    }
+
+    componentDidMount() {
+        this.setState({
+            city_name: this.props.city
+        })
+    }
+
+
+    render() {
+
+
+
+        return (
+            <div>
+
+
+                <div className="container">
+                    <h2 className="text-center city-info">{this.props.city}, {this.props.country}, {this.props.localtime} </h2>
+                    <div className="d-flex justify-content-around row">
+                        {this.props.weather.map((day, i) => (
+                            <div className="col-lg-2 gun-card" key={i} >
+                                <div className="card shadow-sm" style={{ border: "solid 2px", borderRadius: "5%" }}>
+                                    <img src={day.day.condition.icon} className="card-img-top" style={{ height: 105, width: 105, margin: "0 auto" }} alt="..." />
+                                    <div className="d-flex flex-column card-body">
+                                        <h4 className="card-title text-center">{day.date.split('-').reverse().join('/')}</h4>
+                                        <hr />
+                                        <h6 className="card-title text-center">{day.day.condition.text}</h6>
+                                        <p className="card-text text-center">Avarage Temperature: {day.day.avgtemp_c} °C</p>
+                                        <p className="card-text text-center">Maximum Temperature: {day.day.maxtemp_c} °C</p>
+                                        <p className="card-text text-center">Minimum Temperature: {day.day.mintemp_c} °C</p>
+                                        <p className="card-text text-center">Avarage Humidity: %{day.day.avghumidity} </p>
+                                        <p className="card-text text-center">Maximum Wind: {day.day.maxwind_kph} km/hour </p>
+                                    </div>
+
+
+
+                                </div>
+
+                            </div>
+                        )
+
+                        )}
+
+
+
+
+                    </div>
+                </div>
+
+            </div>
+
+        )
+    }
+}
+
+export default DayList;
---
+++ src/components/SearchBar.js
@@ -0,0 +1,111 @@
+import React from 'react'
+import DayList from '../components/DayList'
+import logo from '../img/logo.png'
+import axios from 'axios'
+import '../index.css'
+
+let textInput = React.createRef();
+class SearchBar extends React.Component {
+
+
+
+    state = {
+        country: "",
+        city_value: "",
+        weather: [],
+        localtime: "",
+        init: 0
+    }
+
+
+
+    onSubmit = (e) => {
+        e.preventDefault();
+        //console.log(textInput.current.value)
+        this.onChange(textInput.current.value)
+        textInput.current.value = ""
+    }
+
+    onChange = async (city_value) => {
+        const response = await axios.get(`http://api.weatherapi.com/v1/forecast.json?key=28193ed4b743490692a92524212408&q=${city_value}&days=3`)
+        //console.log(response.data.location.name)
+        this.setState({
+            weather: response.data.forecast.forecastday,
+            localtime: response.data.location.localtime.split(" ")[1],
+            country: response.data.location.country,
+            city_value: response.data.location.name
+        })
+
+    }
+
+    handleKeyPress = async (e) => {
+
+        if (e.key === "Enter") {
+            //console.log(e.target.value)
+            e.preventDefault();
+            const response = await axios.get(`http://api.weatherapi.com/v1/forecast.json?key=28193ed4b743490692a92524212408&q=${e.target.value}&days=3`)
+            //console.log(response.data.location.name)
+            this.setState({
+                weather: response.data.forecast.forecastday,
+                localtime: response.data.location.localtime.split(" ")[1],
+                country: response.data.location.country,
+                city_value: response.data.location.name
+            })
+            e.target.value = "";
+        }
+    }
+
+
+    async componentDidMount() {
+        const response = await fetch("https://ip-geo-location.p.rapidapi.com/ip/check?format=json", {
+            "method": "GET",
+            "headers": {
+                "x-rapidapi-host": "ip-geo-location.p.rapidapi.com",
+                "x-rapidapi-key": "a72edcbfaamsh96bf1fc7aee8f23p1c27fajsn74d1a5ffa4a4"
+            }
+        })
+        const initial = await response.json();
+        //console.log(initial.city.name)
+        this.setState({
+            city_value: initial.city.name,
+            country: initial.country.name,
+            init: 1
+
+        })
+       // console.log(initial)
+
+        const response2 = await axios.get(`http://api.weatherapi.com/v1/forecast.json?key=28193ed4b743490692a92524212408&q=${this.state.city_value}&days=3`)
+
+        this.setState({
+            weather: response2.data.forecast.forecastday,
+            localtime: response2.data.location.localtime.split(" ")[1],
+            //country: response2.data.location.country
+
+        })
+    }
+
+
+
+
+    render() {
+
+
+        return (this.state.init ?
+            <div>
+
+                <div className="text-center">
+                    <img src={logo} className="rounded" alt="..." />
+                </div>
+                <form>
+                    <input ref={textInput}  type="text" className="search-bar" placeholder="Write the city name to see its forecast" onFocus={(e) => { e.target.placeholder = "" }} onBlur={(e) => { e.target.placeholder = "Write the city name to see its forecast" }} onKeyPress={this.handleKeyPress} />
+                </form>
+                <button style={{ display: "block", margin: "1rem auto" }} className="search-button" onClick={this.onSubmit}><span>Look For It!</span></button>
+
+                <DayList city={this.state.city_value} weather={this.state.weather} localtime={this.state.localtime} country={this.state.country} />
+            </div> : <div></div>
+
+        )
+    }
+}
+
+export default SearchBar
